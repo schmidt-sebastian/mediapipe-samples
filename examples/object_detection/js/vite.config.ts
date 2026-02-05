69383488e060088c9e5409680211b750ba9a73fc
Sebastian Schmidt <mrschmidt@google.com>
2026-02-05 10:11:26 -0700

    Web demo for ObjectDetector

---
+++ examples/object_detection/js/vite.config.ts
@@ -0,0 +1,16 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/@mediapipe/tasks-vision/wasm',
+          dest: '.'
+        }
+      ]
+    })
+  ]
+});
